edcd31ef9c43c611750c57f800bff0cbf4adcea7
Samuel Sasaki <64754486+sammsaski@users.noreply.github.com>
2024-12-05 07:51:08 -0600

    small fixes to the demos

--- translation/pages/1_Calculator.py
+++ translation/pages/1_Calculator.py
@@ -24,6 +24,10 @@ from torch.utils.data import DataLoader, Dataset, SequentialSampler, RandomSampl
 from torch.utils.data.distributed import DistributedSampler
 from transformers import (WEIGHTS_NAME, AdamW, get_linear_schedule_with_warmup,
                           RobertaConfig, RobertaModel, RobertaTokenizer)
+from parser import (remove_comments_and_docstrings,
+                   tree_to_token_index,
+                   index_to_code_token,
+                   tree_to_variable_index)
 
 import inspect
 
@@ -143,6 +147,126 @@ model.eval()
 
 print("Finished loading the model!")
 
+def extract_dataflow(code, parser,lang):
+    #remove comments
+    try:
+        code=remove_comments_and_docstrings(code,lang)
+    except:
+        pass    
+    #obtain dataflow
+    if lang=="php":
+        code="<?php"+code+"?>"    
+    try:
+        tree = parser[0].parse(bytes(code,'utf8'))    
+        root_node = tree.root_node  
+        tokens_index=tree_to_token_index(root_node)     
+        code=code.split('\n')
+        code_tokens=[index_to_code_token(x,code) for x in tokens_index]  
+        index_to_code={}
+        for idx,(index,code) in enumerate(zip(tokens_index,code_tokens)):
+            index_to_code[index]=(idx,code)  
+        try:
+            DFG,_=parser[1](root_node,index_to_code,{}) 
+        except:
+            DFG=[]
+        DFG=sorted(DFG,key=lambda x:x[1])
+        indexs=set()
+        for d in DFG:
+            if len(d[-1])!=0:
+                indexs.add(d[1])
+            for x in d[-1]:
+                indexs.add(x)
+        new_DFG=[]
+        for d in DFG:
+            if d[1] in indexs:
+                new_DFG.append(d)
+        dfg=new_DFG
+    except:
+        dfg=[]
+    return code_tokens,dfg
+
+def convert_examples_to_features(examples, tokenizer, args,stage=None):
+    features = []
+    for example_index, example in enumerate(tqdm(examples,total=len(examples))):
+        ##extract data flow
+        code_tokens,dfg=extract_dataflow(example.source,
+                                         parsers["c_sharp" if args.source_lang == "cs" else "java"],
+                                         "c_sharp" if args.source_lang == "cs" else "java")
+        code_tokens=[tokenizer.tokenize('@ '+x)[1:] if idx!=0 else tokenizer.tokenize(x) for idx,x in enumerate(code_tokens)]
+        ori2cur_pos={}
+        ori2cur_pos[-1]=(0,0)
+        for i in range(len(code_tokens)):
+            ori2cur_pos[i]=(ori2cur_pos[i-1][1],ori2cur_pos[i-1][1]+len(code_tokens[i]))    
+        code_tokens=[y for x in code_tokens for y in x]  
+        
+        #truncating
+        code_tokens=code_tokens[:args.max_source_length-3][:512-3]
+        source_tokens =[tokenizer.cls_token]+code_tokens+[tokenizer.sep_token]
+        source_ids =  tokenizer.convert_tokens_to_ids(source_tokens)
+        position_idx = [i+tokenizer.pad_token_id + 1 for i in range(len(source_tokens))]
+        dfg=dfg[:args.max_source_length-len(source_tokens)]
+        source_tokens+=[x[0] for x in dfg]
+        position_idx+=[0 for x in dfg]
+        source_ids+=[tokenizer.unk_token_id for x in dfg]
+        padding_length=args.max_source_length-len(source_ids)
+        position_idx+=[tokenizer.pad_token_id]*padding_length
+        source_ids+=[tokenizer.pad_token_id]*padding_length      
+        source_mask = [1] * (len(source_tokens))
+        source_mask+=[0]*padding_length        
+        
+        #reindex
+        reverse_index={}
+        for idx,x in enumerate(dfg):
+            reverse_index[x[1]]=idx
+        for idx,x in enumerate(dfg):
+            dfg[idx]=x[:-1]+([reverse_index[i] for i in x[-1] if i in reverse_index],)    
+        dfg_to_dfg=[x[-1] for x in dfg]
+        dfg_to_code=[ori2cur_pos[x[1]] for x in dfg]
+        length=len([tokenizer.cls_token])
+        dfg_to_code=[(x[0]+length,x[1]+length) for x in dfg_to_code]        
+      
+
+        #target
+        if stage=="test":
+            target_tokens = tokenizer.tokenize("None")
+        else:
+            target_tokens = tokenizer.tokenize(example.target)[:args.max_target_length-2]
+        target_tokens = [tokenizer.cls_token]+target_tokens+[tokenizer.sep_token]            
+        target_ids = tokenizer.convert_tokens_to_ids(target_tokens)
+        target_mask = [1] *len(target_ids)
+        padding_length = args.max_target_length - len(target_ids)
+        target_ids+=[tokenizer.pad_token_id]*padding_length
+        target_mask+=[0]*padding_length   
+   
+        if example_index < 5:
+            if stage=='train':
+                logger.info("*** Example ***")
+                logger.info("source_tokens: {}".format([x.replace('\u0120','_') for x in source_tokens]))
+                logger.info("source_ids: {}".format(' '.join(map(str, source_ids))))
+                logger.info("source_mask: {}".format(' '.join(map(str, source_mask))))
+                logger.info("position_idx: {}".format(position_idx))
+                logger.info("dfg_to_code: {}".format(' '.join(map(str, dfg_to_code))))
+                logger.info("dfg_to_dfg: {}".format(' '.join(map(str, dfg_to_dfg))))
+                
+                logger.info("target_tokens: {}".format([x.replace('\u0120','_') for x in target_tokens]))
+                logger.info("target_ids: {}".format(' '.join(map(str, target_ids))))
+                logger.info("target_mask: {}".format(' '.join(map(str, target_mask))))
+       
+        features.append(
+            InputFeatures(
+                 example_index,
+                 source_ids,
+                 position_idx,
+                 dfg_to_code,
+                 dfg_to_dfg,
+                 target_ids,
+                 source_mask,
+                 target_mask,
+            )
+        )
+    return features
+
+
 def model_infer(sample_code):
     """Use the code translation model
 
@@ -236,7 +360,7 @@ if st.button("Translate"):
     # Add the expected code
     st.header("Target C# Code")
     expected_code_output = code_editor(
-        "public class Calculator { public static int Add(int a, int b) { return a + b; } }", lang="csharp"
+        "public static int Add(int a, int b) { return a + b; }", lang="csharp"
     )
 
 #
